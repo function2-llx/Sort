745ee276e503f5cc62c7b1a5657a9ea98f76f26a
illyasviel3 <illyyasviel@qq.com>
2018-10-31 22:06:48 +0800

    quick

--- src/Simulator.cpp
+++ src/Simulator.cpp
@@ -15,8 +15,34 @@ void Simulator::sort(int* l, int* r)
 	int n = r - l;
 	if (n <= 1)
 		return;
-    int *pivot=l+rand()%n;
-    std::swap(*l,*pivot);
+    int step=1,upper;
+    if (n>32){
+        upper=9;
+    }else{
+        if (n>25){
+            upper=3;
+        }else{
+            upper=1;
+        }
+    }
+    int *rr=l+9;
+    while (step<upper){
+        for (int *i=l,*j=i+step,*k=j+step;k<rr;i=k+step,j=i+step,k=j+step){
+            if (!cmp(*i,*j)){
+                std::swap(*i,*j);
+            }
+            if (cmp(*i,*k)){
+                if (cmp(*j,*k)){
+                    std::swap(*i,*j);
+                }else{
+                    std::swap(*i,*k);
+                }
+            }
+        }
+        step*=3;
+    }
+    //int *pivot=l+rand()%(n/step)*step;
+    //std::swap(*l,*pivot);
 	int *i = l, *j = r - 1, val = *l;
 	while (i < j) {
         while (i<j && !cmp(*j,val)){
